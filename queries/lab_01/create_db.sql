c8364cb9e21e2eedd99beab92f6ccc5c77a71876
Simeon Romanov <archeosAnacritis@gmail.com>
2022-09-17 02:21:52 +0300

    lab_01 queries update

--- queries/lab_01/create_db.sql
+++ queries/lab_01/create_db.sql
@@ -3,46 +3,43 @@ CREATE DATABASE military_base;
 \c military_base;
 
 CREATE TABLE IF NOT EXISTS soldiers (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    rank VARCHAR(50) NOT NULL,
-    age INT NOT NULL,
-    salary INT NOT NULL,
-    arrival_date DATE NOT NULL,
+    id SERIAL UNIQUE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    rank VARCHAR(50),
+    age INT,
+    salary INT,
+    arrival_date DATE,
     departure_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS weapons(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    classification VARCHAR(50) NOT NULL,
-    caliber VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    produced DATE NOT NULL,
-    last_check DATE NOT NULL,
-    soldier_id INT,
-    FOREIGN KEY (soldier_id) REFERENCES soldiers(id)
+    id SERIAL,
+    name VARCHAR(50),
+    classification VARCHAR(50),
+    caliber VARCHAR(50),
+    status VARCHAR(50),
+    produced DATE,
+    last_check DATE,
+    attached_soldier_id INT
 );
 
 CREATE TABLE IF NOT EXISTS ammunition(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    caliber VARCHAR(50) NOT NULL,
-    type VARCHAR(50) NOT NULL,
+    id SERIAL,
+    name VARCHAR(50),
+    caliber VARCHAR(50),
+    type VARCHAR(50),
     quantity INT DEFAULT 0,
-    status VARCHAR(50) NOT NULL,
-    soldier_id INT,
-    FOREIGN KEY (soldier_id) REFERENCES soldiers(id)
+    status VARCHAR(50),
+    attached_soldier_id INT
 );
 
 CREATE TABLE IF NOT EXISTS vehicles(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    capacity INT NOT NULL,
-    armored BOOLEAN NOT NULL,
-    produced DATE NOT NULL,
-    soldier_id INT,
-    FOREIGN KEY (soldier_id) REFERENCES soldiers(id)
+    id SERIAL,
+    name VARCHAR(50),
+    status VARCHAR(50),
+    capacity INT,
+    armored BOOLEAN,
+    produced DATE,
+    attached_soldier_id INT
 );
--- queries/lab_01/mass_copy.sql
+++ queries/lab_01/mass_copy.sql
@@ -1,5 +1,18 @@
-
+/*psql -h localhost -p 5432 -U postgres -f mass_copy.sql */
 \c military_base;
+SELECT * FROM soldiers;
+ALTER TABLE weapons DROP CONSTRAINT F_AWID;
+ALTER TABLE ammunition DROP CONSTRAINT F_AAID;
+ALTER TABLE vehicles DROP CONSTRAINT F_AVID;
+
+TRUNCATE TABLE weapons;
+TRUNCATE TABLE ammunition;
+TRUNCATE TABLE vehicles;
+TRUNCATE TABLE soldiers;
+
+ALTER TABLE ammunition ADD CONSTRAINT F_AAID FOREIGN KEY (attached_soldier_id) REFERENCES soldiers(id);
+ALTER TABLE vehicles ADD CONSTRAINT F_AVID FOREIGN KEY (attached_soldier_id) REFERENCES soldiers(id);
+ALTER TABLE weapons ADD CONSTRAINT F_AWID FOREIGN KEY (attached_soldier_id) REFERENCES soldiers(id);
 
 \copy soldiers FROM './dbdata/soldiers.csv' DELIMITER ',' CSV HEADER;
 
